92804726e558b88bd02b6ce6ed24eb06480c732e
sombrero104 <sombrero.khy@gmail.com>
2021-02-09 14:33:08 +0900

    .

---
+++ src/main/java/me/sombrero/demorestapi/events/Event.java
@@ -0,0 +1,40 @@
+package me.sombrero.demorestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * @EqualsAndHashCode(of = "id")
+ * equals()와 hashCode()를 구현할 때 기본적으로 모든 필드들을 다 사용한다.
+ * 그런데 만약 엔티티간에 (이 Event 객체가 다른 객체간에) 연관관계가 있을 경우에는
+ * 엔티티간에 서로 상호 참조하는 관계가 되면
+ * eauals()와 hashCode()를 구현한 코드 안에서 스택오버플로우가 발생할 수 있다.
+ * 때문에 여기에서는 id 값만 가지고 equals()와 hashCode()를 서로 비교한다.
+ * 원한다면 다른 필드들을 더 추가해서 비교를 할수도 있다.
+ * 예) @EqualsAndHashCode(of = {"id", "name"})
+ * 하지만 절대로 연관관계에 있는 필드는 추가해서는 안된다.
+ * 예를 들어 Account 객체와 연관관계가 있을 경우
+ * @EqualsAndHashCode(of = {"id", "account"})와 같이
+ * 서로 상호참조하는 필드는 추가하면 스택오버플로우가 발생할 수 있다.
+ */
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional) 이게 없으면 온라인 모임
+    private int basePrice; // (optional)
+    private int maxPrice; // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+}
---
+++ src/main/java/me/sombrero/demorestapi/events/EventStatus.java
@@ -0,0 +1,7 @@
+package me.sombrero.demorestapi.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+
+}
