aec7b475f31eadb56b4833ebe028a40a29d7fef4
sombrero104 <sombrero.khy@gmail.com>
2021-02-12 22:21:29 +0900

    .

--- src/test/java/me/sombrero/demorestapi/configs/AuthServerConfigTest.java
+++ src/test/java/me/sombrero/demorestapi/configs/AuthServerConfigTest.java
@@ -1,13 +1,20 @@
 package me.sombrero.demorestapi.configs;
 
+import me.sombrero.demorestapi.accounts.Account;
+import me.sombrero.demorestapi.accounts.AccountRole;
 import me.sombrero.demorestapi.accounts.AccountService;
 import me.sombrero.demorestapi.common.BaseControllerTest;
 import me.sombrero.demorestapi.common.TestDescription;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Set;
+
 import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class AuthServerConfigTest extends BaseControllerTest {
 
@@ -16,8 +23,29 @@ public class AuthServerConfigTest extends BaseControllerTest {
 
     @Test
     @TestDescription("인증 토큰을 발급 받는 테스트")
-    public void getAuthToken() {
-        this.mockMvc.perform(post("/oauth/token"))
+    public void getAuthToken() throws Exception {
+        // Given
+        String username = "sombrero104@gmail.com";
+        String password = "1234";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret)) // clientId, clientSecret를 가지고 BasicAuth라는 헤더를 만든다.
+                    .param("username", username)
+                    .param("paassword", password)
+                    .param("grant_type", "password")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
     }
 
 }
